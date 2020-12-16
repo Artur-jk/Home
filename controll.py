0dd9e2d9a28dd5227491e4a066e2e92c502c9416
Arthur <artking01kg@gmail.com>
2020-12-17 00:25:03 +0300

    Create controll.py
    Done 3 of 5

---
+++ controll.py
@@ -0,0 +1,29 @@
+
+def merge_list(*args):
+    a = []
+    for val in args:
+        if(list == type(val) or type(val) == set or type(val) == tuple):
+            a.extend(val)
+        else:
+            a.append(val)
+    return a
+
+print(merge_list([0,0], "nice", {5,4,1}, (12,9), None))
+
+
+def remove_duplicates(list_of_lists):
+     print([list(kek) for kek in {tuple(item) for item in list_of_lists}])
+
+remove_duplicates([[10, 20],  [40],  [30, 56, 25], [10, 20],
+[33], [40]])
+
+
+def merge_list(list_to_merge):
+    a = []
+    for i in range(len(list_to_merge)):
+        a.extend(list_to_merge[i])
+    print(a)
+
+merge_list([[1, 8, 3], [-5, 0], [4], [2, 3, 3]])
+
+
